b1e9f15c73545cc370b6d6a208e323f3e2df7417
mlfauzi54@gmail.com <mlfauzi54@gmail.com>
2022-05-12 13:22:12 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connection Succesfull"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/products", productRoute);
+app.use("/api/v1/carts", cartRoute);
+app.use("/api/v1/orders", orderRoute);
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
